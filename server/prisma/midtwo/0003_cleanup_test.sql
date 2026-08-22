ae2642abc18d626ceddcbc33c4392c0ab388780d
liixnglinb <lixingli1024@qq.com>
2026-08-22 13:21:34 +0800

    chore: 移除废弃后端与阿里云部署目录，改用纯 Supabase 架构

--- server/prisma/midtwo/0003_cleanup_test.sql
+++
@@ -1,5 +0,0 @@
--- 清理多用户测试数据（删除测试账号及其隔离数据）
-DELETE FROM "kv" WHERE "user_id" IN (
-  SELECT "id" FROM "users" WHERE "email" IN ('test2@user.com', 'user3@user.com')
-);
-DELETE FROM "users" WHERE "email" IN ('test2@user.com', 'user3@user.com');
